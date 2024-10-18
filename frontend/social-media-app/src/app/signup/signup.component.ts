5094a9896fb05f15a8ad2c247a5b213af7bc5ed5
Bhuvanesh_nisant <bhuvane423@gmail.com>
2024-10-18 18:32:42 +0530

    signup chaged

--- frontend/social-media-app/src/app/signup/signup.component.ts
+++ frontend/social-media-app/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -13,15 +14,48 @@ export class SignupComponent {
   confirmPassword: string = '';
   mobileNo: string = '';
 
+  passwordMismatch: boolean = false; 
+
+  constructor(private http: HttpClient) {}
+
   onSubmit(form: NgForm) {
     if (this.password !== this.confirmPassword) {
-      alert('Password do not match');
-      return;
+      this.passwordMismatch = true;
+      return; 
+    } else {
+      this.passwordMismatch = false;
     }
 
-    if (form.valid) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
 
-      console.log('Form Submitted!', form.value);
-    }
+    const signupData = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      mobileNo: this.mobileNo
+    };
+
+    this.http.post('http://localhost:8080/signup', signupData, { headers })
+      .subscribe({
+        next: (response) => {
+          console.log('Signup successful', response);
+          alert('Signup successful!');
+          this.resetForm(form);
+        },
+        error: (err) => {
+          console.log('Signup failed', err);
+        }
+      });
+  }
+
+  resetForm(form: NgForm) {
+    form.reset();
+    this.name = '';
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.mobileNo = '';
   }
 }
