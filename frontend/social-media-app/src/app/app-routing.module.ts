41330c876e579c69df2c4719e493ac55d6651643
Bhuvanesh_nisant <bhuvane423@gmail.com>
2024-10-19 11:37:44 +0530

    changed routing

--- frontend/social-media-app/src/app/app-routing.module.ts
+++ frontend/social-media-app/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
+  
   {path:'home',component:HomeComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**',redirectTo:'home',pathMatch:'full'}
 ];
 
 @NgModule({
